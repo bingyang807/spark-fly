ab39d16ee3129abfe62068b6577bf3710a1bfe78
licongyang <liby-1987@163.com>
2022-12-10 22:09:07 +0800

    [docs] init the spark project spark 3.1.2 scala 2.12 java 1.8
    # init scala
    1.idea项目名右键 add support -> 勾选scala
    2. project 配置 -> lib -> browse 本地scala sdk （2.12.16）

---
+++ spark-basic/src/main/java/com/legend/spark/WordCount.scala
@@ -0,0 +1,57 @@
+package com.legend.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+/**
+ *  Word Count 的初衷是对文件中的单词做统计计数，打印出频次最高的 5 个词汇
+ *  包含如下三个步骤：
+ *  1. 读取内容： 调用Spark文件读取API ，加载wikiOfSpark.txt
+ *  2. 分词： 以行为单位，把句子打散为单词
+ *  3. 分组计数： 按照单词分组计数
+ */
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    // create sparksession
+    val spark: SparkSession = SparkSession.builder()
+      .master("local")
+      // .master("local[3]")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    // 这里的下划线"_"是占位符，代表数据文件的根目录
+    val rootPath: String = "/Users/alfie/workspace/code/learn/spark-fly/spark-basic/src/main/resources"
+//     val rootPath: String = _
+    val file: String = s"${rootPath}/wikiOfSpark.txt"
+
+    // 读取文件内容
+    val lineRDD: RDD[String] = sc.textFile(file)
+    println("initial partition count:" + lineRDD.getNumPartitions)
+
+    val reparRdd = lineRDD.repartition(4)
+    println("re-partition count:" + lineRDD.getNumPartitions)
+
+    // 以行为单位分词
+    val wordRDD: RDD[String] = lineRDD.flatMap(line => line.split(" "))
+    wordRDD.foreach(f => println(f))
+    // 过滤空字符串
+    val cleanWordRDD: RDD[String] = wordRDD.filter(word => !word.equals(""))
+    cleanWordRDD.foreach(f => println(f))
+
+    //把RDD元素String，转换为（key, value）的形式
+    val kvRDD: RDD[(String, Int)] = cleanWordRDD.map(word => (word, 1))
+    kvRDD.foreach(println)
+    //按照单词做分组计数
+    val wordCounts: RDD[(String, Int)] = kvRDD.reduceByKey(_ + _)
+//    val wordCounts: RDD[(String, Int)] = kvRDD.reduceByKey((x, y) => x + y)
+    wordCounts.foreach(println)
+
+    // 打印词频最高的5个词汇
+    val top5Word = wordCounts.map { case (k, v) => (k, v) }.sortByKey(false).take(5)
+    top5Word.foreach(println)
+
+    wordCounts.saveAsTextFile("/Users/alfie/workspace/code/learn/spark-fly/spark-basic/src/main/resources/wordcount.txt")
+
+  }
+
+}
